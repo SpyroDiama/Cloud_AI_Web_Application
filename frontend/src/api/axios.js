1b8434d76a70abd2811cf3e148b4f7103620c6f1
SpyroDiama <spurosdiamanths@gmail.com>
2026-05-07 01:29:10 +0300

    final configs before digitalocean deploy

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:8000/api/';
+const baseURL = process.env.REACT_APP_API_URL || 'http://localhost:8000/api/';
 
 const axiosInstance = axios.create({
     baseURL: baseURL,
