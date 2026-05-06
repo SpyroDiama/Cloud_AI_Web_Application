0a79f900083e454463d5d069b25fc1bb6465e0b7
SpyroDiama <spurosdiamanths@gmail.com>
2026-05-07 01:27:32 +0300

    final config steps for deployment to digital ocean

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -79,7 +79,10 @@ REST_FRAMEWORK = {
     ),
 }
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://your-droplet-ip",
+]
 
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
